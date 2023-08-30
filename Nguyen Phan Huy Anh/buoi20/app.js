b42ecd583330475080fec8a06624ea7c7361a7c9
huyanh <58281984+huyanh517@users.noreply.github.com>
2023-08-30 21:17:33 +0700

    Bai tap

--- Nguyen Phan Huy Anh/buoi20/app.js
+++ Nguyen Phan Huy Anh/buoi20/app.js
@@ -1,4 +1,4 @@
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var arr = [1, 2, 3, 4, 5, 11, 6, 7, 8, 9, 10];
 
 var arrSoChan = [];
 var arrSoLe = [];
@@ -24,3 +24,26 @@ for (var i = 0; i < arr.length; i++) {
 }
 
 console.log("Tong cua mang la " + sum);
+
+// Tim so lon nhat trong mang
+
+
+var x;
+for (var i = 0; i < arr.length - 1; i++) {
+    for (var j = i + 1; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+            x = arr[i];
+            arr[i] = arr[j];
+            arr[j] = x;
+        }
+    }
+}
+
+console.log("So lon nhat la " + arr[arr.length - 1])
+
+// TIm so nho nhat
+
+console.log("So nho nhat la " + arr[0])
+
+
+//
