e3927da0c2985c26947977ad32510e8ef465b0a0
huyanh <58281984+huyanh517@users.noreply.github.com>
2023-08-30 21:31:14 +0700

    Bai tap

--- Nguyen Phan Huy Anh/buoi20/app.js
+++ Nguyen Phan Huy Anh/buoi20/app.js
@@ -27,17 +27,25 @@ console.log("Tong cua mang la " + sum);
 
 // Tim so lon nhat trong mang
 
-
 var x;
-for (var i = 0; i < arr.length - 1; i++) {
-    for (var j = i + 1; j < arr.length; j++) {
-        if (arr[i] > arr[j]) {
-            x = arr[i];
-            arr[i] = arr[j];
-            arr[j] = x;
+
+// Sap xep mang theo thu tu tang dan
+
+function sapXepTangDan(array) {
+    for (var i = 0; i < array.length - 1; i++) {
+        for (var j = i + 1; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                x = array[i];
+                array[i] = array[j];
+                array[j] = x;
+            }
         }
     }
+    return array;
 }
+
+sapXepTangDan(arr);
+
 
 console.log("So lon nhat la " + arr[arr.length - 1])
 
@@ -46,4 +54,32 @@ console.log("So lon nhat la " + arr[arr.length - 1])
 console.log("So nho nhat la " + arr[0])
 
 
-//
+// Bang cuu chuong
+
+for (var x = 1; x <= 10; x++) {
+    for (var y = 1; y <= 10; y++) {
+        console.log(x + ' x ' + y + ' = ' + x * y);
+    }
+}
+
+// Tim so lon nhat giua 2 mang
+
+var arr1 = [1, 2 ,3];
+var arr2 = [5,6,7,8];
+
+var maxArr1 = sapXepTangDan(arr1)[arr1.length - 1]
+var maxArr2 = sapXepTangDan(arr2)[arr2.length - 1]
+
+function soSanh(number1, number2) {
+    if (number1 > number2) {
+        return number1
+    } else if (number1 < number2) {
+        return number2
+    } else {
+        return number1;
+    }
+}
+
+console.log("So lon nhat cua 2 mang la " + soSanh(maxArr1, maxArr2))
+
+
