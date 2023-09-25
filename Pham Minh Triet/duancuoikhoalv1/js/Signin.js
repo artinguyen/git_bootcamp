fff38640d7303145442d34852e8a5334381f9dcf
trietpham252 <142817355+trietpham252@users.noreply.github.com>
2023-09-25 21:23:00 +0700

    db

--- Pham Minh Triet/duancuoikhoalv1/js/Signin.js
+++ Pham Minh Triet/duancuoikhoalv1/js/Signin.js
@@ -50,7 +50,7 @@
 //       displayError("sexError", "Vui lòng chọn giới tính.");
 //       return false;
 //     }
-  
+
 //     // Kiểm tra tính hợp lệ của ô #nameuser
 //     if (usernameInput.trim() === "") {
 //       displayError("nameuserError", "");
@@ -102,130 +102,178 @@
 //     }
 // }
 // viết bằng j query
-function validateForm() {
-    // Lấy giá trị của các ô input
-    let nameInput = $("#name").val();
-    let emailInput = $("#email").val();
-    let phoneNumberInput = $("#phonenumber").val();
-    let sexInputs = $("input[name='sex']");
-    let usernameInput = $("#nameuser").val();
-    let passwordInput = $("#password").val();
-    let confirmPasswordInput = $("#confirmpassword").val();
-  
-    // Xóa thông báo lỗi cũ
-    clearErrors();
-  
-    // Kiểm tra tên
-    if ($.trim(nameInput) === "") {
-      displayError("nameError", "Họ và tên không được bỏ trống.");
-      return false;
-    } else if (nameInput.length < 5) {
-      displayError("nameError", "Họ và tên phải có ít nhất 5 ký tự.");
-      return false;
-    }
-  
-    // Kiểm tra số điện thoại
-    if ($.trim(phoneNumberInput) === "") {
-      displayError("phonenumberError", "Số điện thoại không được bỏ trống.");
-      return false;
-    } else if (
-      phoneNumberInput.length < 10 ||
-      phoneNumberInput.length > 12 ||
-      phoneNumberInput.charAt(0) !== "0"
-    ) {
-      displayError("phonenumberError", "Số điện thoại không hợp lệ.");
-      return false;
-    }
-  
-    // Kiểm tra email
-    if ($.trim(emailInput) === "") {
-      displayError("emailError", "Email không được bỏ trống.");
-      return false;
-    } else if (!emailInput.includes("@") || !emailInput.includes(".com")) {
-      displayError("emailError", "Email không hợp lệ.");
-      return false;
-    }
-  
-    // Kiểm tra giới tính
-    var selectedSex = false;
-    sexInputs.each(function () {
-      if ($(this).is(":checked")) {
-        selectedSex = true;
-        return false;
-      }
-    });
-    if (!selectedSex) {
-      displayError("sexError", "Vui lòng chọn giới tính.");
-      return false;
-    }
-  
-    // Kiểm tra tên người dùng
-    if ($.trim(usernameInput) === "") {
-      displayError("nameuserError", "");
-      return false;
-    } else if (usernameInput.length < 6) {
-      displayError(
-        "nameuserError",
-        "Tên người dùng phải có ít nhất 6 ký tự."
-      );
-      return false;
-    }
-  
-    // Kiểm tra mật khẩu
-    if ($.trim(passwordInput) === "") {
-      displayError(
-        "passwordError",
-        "Mật khẩu phải có ít nhất 8 ký tự."
-      );
-      return false;
-    } else if (
-      passwordInput.length < 8 ||
-      !/[A-Z]/.test(passwordInput) ||
-      !/[a-z]/.test(passwordInput) ||
-      !/[@#$&]/.test(passwordInput) ||
-      !/\d/.test(passwordInput)
-    ) {
-      displayError(
-        "passwordError",
-        "Mật khẩu không đáp ứng yêu cầu bảo mật. Mật khẩu phải có chứa ít nhất 8 ký tự, chứa ít nhất 1 ký tự viết hoa, 1 ký tự viết thường, 1 ký tự đặt biệt trong dãy (@#$&) và 1 ký tự số."
-      );
-      return false;
-    }
-  
-    // Kiểm tra mật khẩu xác nhận
-    if (confirmPasswordInput !== passwordInput) {
-      displayError(
-        "confirmpasswordError",
-        "Mật khẩu xác nhận không khớp."
-      );
-      return false;
-    }
-  
-    // Nếu tất cả các ô input hợp lệ, hiển thị thông báo thành công
-    alert("Chúc mừng bạn đã đăng ký thành công!");
-    return true;
+function Signin() {
+  let nameInput = $("#name").val();
+  let emailInput = $("#email").val();
+  let phoneNumberInput = $("#phonenumber").val();
+  let sexInputs = $("input[name='sex']");
+  let usernameInput = $("#nameuser").val();
+  let passwordInput = $("#password").val();
+  let confirmPasswordInput = $("#confirmpassword").val();
+
+  // Xóa thông báo lỗi cũ
+  clearErrors();
+
+  // Kiểm tra tên
+  if ($.trim(nameInput) === "") {
+    displayError("nameError", "Họ và tên không được bỏ trống.");
+    return false;
+  } else if (nameInput.length < 5) {
+    displayError("nameError", "Họ và tên phải có ít nhất 5 ký tự.");
+    return false;
   }
-  
-  function displayError(elementId, errorMessage) {
-    var errorElement = $("#" + elementId);
-    errorElement.text(errorMessage);
-    errorElement.show();
-    errorElement.css({
-      color: "red",
-      marginTop: "5px",
-      fontSize: "14px",
-    });
-  
-    // Tìm ô input tương ứng
-    var inputElement = $("#" + elementId.replace("Error", ""));
-    if (inputElement){
-      inputElement.addClass("error-input");
+
+  // Kiểm tra số điện thoại
+  if ($.trim(phoneNumberInput) === "") {
+    displayError("phonenumberError", "Số điện thoại không được bỏ trống.");
+    return false;
+  } else if (
+    phoneNumberInput.length < 10 ||
+    phoneNumberInput.length > 12 ||
+    phoneNumberInput.charAt(0) !== "0"
+  ) {
+    displayError("phonenumberError", "Số điện thoại không hợp lệ.");
+    return false;
+  }
+
+  // Kiểm tra email
+  if ($.trim(emailInput) === "") {
+    displayError("emailError", "Email không được bỏ trống.");
+    return false;
+  } else if (!emailInput.includes("@") || !emailInput.includes(".com")) {
+    displayError("emailError", "Email không hợp lệ.");
+    return false;
+  }
+
+  // Kiểm tra giới tính
+  var selectedSex = false;
+  sexInputs.each(function () {
+    if ($(this).is(":checked")) {
+      selectedSex = true;
+      return false;
     }
+  });
+  if (!selectedSex) {
+    displayError("sexError", "Vui lòng chọn giới tính.");
+    return false;
   }
-  
-  function clearErrors() {
-    $(".error").each(function () {
-      $(this).text("");
-      $(this).hide();
-    });
+
+  // Kiểm tra tên người dùng
+  if ($.trim(usernameInput) === "") {
+    displayError("nameuserError", "");
+    return false;
+  } else if (usernameInput.length < 6) {
+    displayError("nameuserError", "Tên người dùng phải có ít nhất 6 ký tự.");
+    return false;
   }
+
+  // Kiểm tra mật khẩu
+  if ($.trim(passwordInput) === "") {
+    displayError("passwordError", "Mật khẩu phải có ít nhất 8 ký tự.");
+    return false;
+  } else if (
+    passwordInput.length < 8 ||
+    !/[A-Z]/.test(passwordInput) ||
+    !/[a-z]/.test(passwordInput) ||
+    !/[@#$&]/.test(passwordInput) ||
+    !/\d/.test(passwordInput)
+  ) {
+    displayError(
+      "passwordError",
+      "Mật khẩu không đáp ứng yêu cầu bảo mật. Mật khẩu phải có chứa ít nhất 8 ký tự, chứa ít nhất 1 ký tự viết hoa, 1 ký tự viết thường, 1 ký tự đặt biệt trong dãy (@#$&) và 1 ký tự số."
+    );
+    return false;
+  }
+
+
+  if (confirmPasswordInput !== passwordInput) {
+    displayError("confirmpasswordError", "Mật khẩu xác nhận không khớp.");
+    return false;
+  }
+
+  var user = {
+    name: nameInput,
+    email: emailInput,
+    phoneNumber: phoneNumberInput,
+    sex: $("input[name='sex']:checked").val(),
+    username: usernameInput,
+    password: passwordInput
+  };
+
+
+  var request = indexedDB.open("myDatabase", 1);
+
+  request.onerror = function(event) {
+    console.log("Lỗi kết nối tới cơ sở dữ liệu IndexedDB");
+  };
+
+  request.onsuccess = function(event) {
+    var db = event.target.result;
+
+
+    var transaction = db.transaction(["users"], "readwrite");
+
+ 
+    var usersStore = transaction.objectStore("users");
+
+
+    var addUserRequest = usersStore.add(user);
+
+    addUserRequest.onsuccess = function(event) {
+      console.log("Lưu trữ thành công vào IndexedDB");
+    };
+
+    addUserRequest.onerror = function(event) {
+      console.log("Lỗi khi lưu trữ vào IndexedDB");
+    };
+
+
+    transaction.oncomplete = function(event) {
+      console.log("Giao dịch hoàn tất");
+    };
+
+    transaction.onerror = function(event) {
+      console.log("Lỗi giao dịch");
+    };
+
+    db.close();
+  };
+
+  request.onupgradeneeded = function(event) {
+    var db = event.target.result;
+
+ 
+    if (!db.objectStoreNames.contains("users")) {
+      db.createObjectStore("users", { keyPath: "username" });
+    }
+  };
+
+  alert("Chúc mừng bạn đã đăng ký thành công!");
+  return true;
+}
+
+
+
+function displayError(elementId, errorMessage) {
+  var errorElement = $("#" + elementId);
+  errorElement.text(errorMessage);
+  errorElement.show();
+  errorElement.css({
+    color: "red",
+    marginTop: "5px",
+    fontSize: "14px",
+  });
+
+  // Tìm ô input tương ứng
+  var inputElement = $("#" + elementId.replace("Error", ""));
+  if (inputElement) {
+    inputElement.addClass("error-input");
+  }
+}
+
+function clearErrors() {
+  $(".error").each(function () {
+    $(this).text("");
+    $(this).hide();
+  });
+}
